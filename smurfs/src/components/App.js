5e4d20c343dd1089692bd01db23a6d3e6ec17b2e
Junior Dugue <jrdugue@gmail.com>
2019-07-20 19:26:46 -0400

    forgot to route to smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -28,7 +28,7 @@ class App extends Component {
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div> */}
         <Route exact path="/" component={ Dashboard }/>
-        <Route exact path="/" component={ SmurfsList }/>
+        <Route exact path="/smurfs" component={ SmurfsList }/>
         <Route path="/form" component={ Form }/>
       </div>
     );
